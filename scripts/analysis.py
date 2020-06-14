598734127ff9f9c8d3a03033af3d0effc035c610
date_cocoa <co.yo.fo@gmail.com>
2020-06-14 19:55:12 +0900

    organaize directory

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -5,7 +5,10 @@ import os
 
 # home効果を可視化
 path = os.getcwd()
-data = pd.read_csv(path + '/' + 'data.csv')
+parent_path = '/'.join(path.split('/')[:-1])
+
+path = os.getcwd()
+data = pd.read_csv(parent_path + '/' + 'data/' + 'data.csv')
 data = data[data['year'] > 1998] # pk戦が廃止以降
 home_sum = sum(data['home_scores'].astype('int'))
 away_sum = sum(data['away_scores'].astype('int'))
@@ -15,4 +18,13 @@ sns.set()
 fig = plt.figure()
 sns.barplot(x=plot_data['Kind'], y=plot_data['Score'])
 plt.xlabel('')
-fig.savefig(path + '/' + 'effect_home.png')
+
+data_save_path = parent_path + '/result/'
+
+try:
+    os.mkdir(data_save_path)
+except FileExistsError as e:
+    print(e)
+    print('既にファイルは存在しているので、os.mkdirの処理はスキップします')
+
+fig.savefig(data_save_path + '/' + 'effect_home.png')
--- scripts/get_data.py
+++ scripts/get_data.py
@@ -2,6 +2,10 @@ import pandas as pd
 import time
 import numpy as np
 import scrape
+import os
+
+path = os.getcwd()
+parent_path = '/'.join(path.split('/')[:-1])
 
 years = np.arange(1993, 2020, 1)
 data = pd.DataFrame()
@@ -11,5 +15,14 @@ for year in years:
     time.sleep(5)
 
 data.columns = ['home', 'score', 'away', 'home_scores', 'away_scores', 'year']
-data.to_csv('data.csv', index=False)
+
+data_save_path = parent_path + '/data/'
+
+try:
+    os.mkdir(data_save_path)
+except FileExistsError as e:
+    print(e)
+    print('既にファイルは存在しているので、os.mkdirの処理はスキップします')
+
+data.to_csv(data_save_path + 'data.csv', index=False)
 print('ファイルを出力しました！')
