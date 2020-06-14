d8cadf1c16c0a7728f1c0a35b289f3ff11664b25
date_cocoa <co.yo.fo@gmail.com>
2020-06-14 20:41:20 +0900

    change scripts

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -6,6 +6,13 @@ import os
 path = os.getcwd()
 parent_path = '/'.join(path.split('/')[:-1])
 
+try:
+    os.mkdir(data_save_path)
+except FileExistsError as e:
+    print(e)
+    print('既にファイルは存在しているので、os.mkdirの処理はスキップします')
+data_save_path = parent_path + '/result/'
+
 # home効果を可視化
 data = pd.read_csv(parent_path + '/' + 'data/' + 'data.csv')
 data = data[data['year'] > 1998] # pk戦が廃止以降
@@ -17,15 +24,6 @@ sns.set()
 fig = plt.figure()
 sns.barplot(x=plot_data['Kind'], y=plot_data['Score'])
 plt.xlabel('')
-
-data_save_path = parent_path + '/result/'
-
-try:
-    os.mkdir(data_save_path)
-except FileExistsError as e:
-    print(e)
-    print('既にファイルは存在しているので、os.mkdirの処理はスキップします')
-
 fig.savefig(data_save_path + '/' + 'effect_home.png')
 print('結果ファイルを出力しました！(effect_home.png)')
 
@@ -34,7 +32,9 @@ data = pd.read_csv(parent_path + '/' + 'data/' + 'data.csv')
 data = data[data['year'] > 1998] # pk戦が廃止以降
 data['total_scores'] = data['home_scores'].astype(int) + data['away_scores'].astype(int)
 
+sns.set()
+fig = plt.figure()
 sns.violinplot(x=data['year'].astype(str), y=data['total_scores'])
-fig.savefig(data_save_path + '/' + 'score_time.png')
-print('結果ファイルを出力しました！(score_time.png)')
+fig.savefig(data_save_path + '/' + 'time_series.png')
+print('結果ファイルを出力しました！(time_series.png)')
 
