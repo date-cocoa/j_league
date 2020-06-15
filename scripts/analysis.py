45e2264fb88841e9dab58d7a48871f12faf9655f
date_cocoa <co.yo.fo@gmail.com>
2020-06-15 19:55:30 +0900

    refactoring analysis.py

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -3,39 +3,57 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 
-# make directory and path
-path = os.getcwd()
-parent_path = '/'.join(path.split('/')[:-1])
-data_save_path = parent_path + '/result/'
+def make_directory(path):
+    """親pathにディレクトリを作成。既に作成されている場合はskip
+    args:
+        path: 現在のpath
+    """
+    parent_path = '/'.join(PATH.split('/')[:-1])
+    data_save_path = parent_path + '/result/'
 
-try:
-    os.mkdir(data_save_path)
-except FileExistsError as e:
-    print(e)
-    print('既にファイルは存在しているので、os.mkdirの処理はスキップします')
+    try:
+        os.mkdir(data_save_path)
+    except FileExistsError as e:
+        print(e)
+        print('既にファイルは存在しているので、os.mkdirの処理はスキップします')
 
-# home効果を可視化
-data = pd.read_csv(parent_path + '/' + 'data/' + 'data.csv')
-data = data[data['year'] > 1998] # pk戦が廃止以降
-home_sum = sum(data['home_scores'].astype('int'))
-away_sum = sum(data['away_scores'].astype('int'))
+def visualize_home_effect(data):
+    """home効果を図示化したを出力（effect_home.png）
+    args:
+        data: dataを入力
+    """
+    home_sum = sum(data['home_scores'].astype('int'))
+    away_sum = sum(data['away_scores'].astype('int'))
+    plot_data = pd.DataFrame({'Score': [home_sum, away_sum], 'Kind': ['Home Team', 'Away Team']})
 
-plot_data = pd.DataFrame({'Score': [home_sum, away_sum], 'Kind': ['Home Team', 'Away Team']})
-sns.set()
-fig = plt.figure()
-sns.barplot(x=plot_data['Kind'], y=plot_data['Score'])
-plt.xlabel('')
-fig.savefig(data_save_path + '/' + 'effect_home.png')
-print('結果ファイルを出力しました！(effect_home.png)')
+    sns.set()
+    fig = plt.figure()
+    sns.barplot(x=plot_data['Kind'], y=plot_data['Score'])
+    plt.xlabel('')
+    fig.savefig(data_save_path + '/' + 'effect_home.png')
+    print('結果ファイルを出力しました！(effect_home.png)')
 
-# 時系列で得点を可視化
-data = pd.read_csv(parent_path + '/' + 'data/' + 'data.csv')
-data = data[data['year'] > 1998] # pk戦が廃止以降
-data['total_scores'] = data['home_scores'].astype(int) + data['away_scores'].astype(int)
+def visualize_timeseries_score(data):
+    """試合の得点を年ごとにviolin plotで可視化（time_series.png）
+    args:
+        data: dataを入力
+    """
+    data['total_scores'] = data['home_scores'].astype(int) + data['away_scores'].astype(int)
 
-sns.set()
-fig = plt.figure(figsize=(10, 7))
-sns.violinplot(x=data['year'].astype(str), y=data['total_scores'])
-fig.savefig(data_save_path + '/' + 'time_series.png')
-print('結果ファイルを出力しました！(time_series.png)')
+    sns.set()
+    fig = plt.figure(figsize=(10, 7))
+    sns.violinplot(x=data['year'].astype(str), y=data['total_scores'])
+    fig.savefig(data_save_path + '/' + 'time_series.png')
+    print('結果ファイルを出力しました！(time_series.png)')
 
+def main():
+    PATH = os.getcwd()
+    data = pd.read_csv(parent_path + '/' + 'data/' + 'data.csv')
+    data = data[data['year'] > 1998] # pk戦が廃止以降
+
+    make_directory(path=PATH)
+    visualize_home_effect(data=data)
+    visualize_home_effect(data=data)
+    
+if __name__ == '__main__':
+    main()
